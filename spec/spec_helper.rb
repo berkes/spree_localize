2b197fd0f480a0f0c11b1820ca109ed6aad995b3
Ryan Bigg <radarlistener@gmail.com>
2012-03-13 09:08:51 -0700

    Customize spec_helper to use Spree::Core::UrlHelpers + set I18n.locale before each test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,28 +5,25 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'rspec/rails'
 
-# Requires supporting ruby files with custom matchers and macros, etc,
-# in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
 
 # Requires factories defined in spree_core
 require 'spree/core/testing_support/factories'
+require 'spree/core/url_helpers'
 
 RSpec.configure do |config|
-  # == Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  config.include Spree::Core::UrlHelpers
   config.mock_with :rspec
-
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  config.before do
+    # Set I18n locale based on test that is running
+    # Directory structure is purpose-built for this
+    #
+    # This part parses out the LOCALE for it
+    # spec/requests/LOCALE/test/goes_here_spec.rb
+    parts = example.file_path.split("/")
+    locale = parts.slice(parts.index("spec")..-1)[2]
+    I18n.locale = Spree::Config[:default_locale] = locale
+  end
 end
