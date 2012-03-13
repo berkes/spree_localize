80c8cc096baf0e2eed67673575991231bb4e2b2f
Ryan Bigg <radarlistener@gmail.com>
2012-03-13 12:04:25 -0700

    'explain' why we have to set cost_price

--- spec/requests/de/admin/products_spec.rb
+++ spec/requests/de/admin/products_spec.rb
@@ -9,6 +9,7 @@ describe "admin products" do
   it "displays price in correct format" do
     visit spree.edit_admin_product_path(product)
     fill_in "product_price", :with => "9,99"
+    # No clue WHY we have to set this, it's just necessary. Ugh.
     fill_in "product_cost_price", :with => "10"
     click_button I18n.t("update")
     Nokogiri::HTML(page.body).css("input#product_price")[0]["value"].should == "9,99"
